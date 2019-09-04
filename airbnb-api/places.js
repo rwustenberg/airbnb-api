ed1f0460dbf07e148433de5656c382fee1cbb496
ruthw <rwustenberg@gmail.com>
2019-09-04 04:08:30 -0500

    080301 080302

---
+++ airbnb-api/places.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+let places = [
+  {
+    _id: "1",
+    title: "Luxury Villa Indu Siam",
+    type: "Entire Villa",
+    price: 200,
+    rating: 5,
+    city: "Koh Samui",
+    country: "Thailand",
+    amenities: ["Kitchen", "Gym", "Iron"],
+    images: [
+      "https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg",
+      "https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg"
+    ]
+  },
+  {
+    _id: "2",
+    title: "Breathtaking Infinite Pool",
+    type: "Entire Villa",
+    price: 180,
+    rating: 4,
+    city: "Bali",
+    country: "Indonesia",
+    amenities: ["Air Conditioning", "Kitchen", "Swimming Pool"],
+    images: [
+      "https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg",
+      "https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg"
+    ]
+  }
+];
+
+app.get("/index", require("Welcome to Airbnb API"));
+app.get("/index", require("./controllers/getPlaces"));
+app.listen(4000, () => {
+  console.log("Ready on port 4000");
+});
---
+++ controllers/root.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+
+module.export = (req, res) => {
+  res.send("List of places");
+};
