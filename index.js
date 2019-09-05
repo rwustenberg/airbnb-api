42a10ac677975b3901c3439cbf99540d77f8f3c7
ruthw <rwustenberg@gmail.com>
2019-09-05 00:51:50 -0500

    090101

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+mongoose.connect(
+  "mongodb://localhost:27017/airbnb",
+  { useNewUrlParser: true },
+  err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to MongoDB");
+    }
+  }
+);
 let places = [
   {
     _id: "1",
