9651962784228007f38e07995c994416ff819c00
ruthw <rwustenberg@gmail.com>
2019-09-10 05:13:46 -0500

    Revert "Revert "100101""
    This reverts commit 56601b3de2d4a0dd566ef342d0b44ef5f013e66d.

--- controllers/getPlaces.js
+++ controllers/getPlaces.js
@@ -1,9 +1,27 @@
 const Place = require("../models/place");
+const Review = require("../models/review");
 
 module.exports = (req, res) => {
   console.log("get places");
   Place.find({})
     .populate("type")
-    .then(data => res.send(data))
+    .populate("host")
+    .select("guests description city country images price title type")
+    .lean()
+    .then(data => {
+      let places = data.map(p => {
+        return Review.find({ place: p._id })
+          .then(reviews => {
+            p.reviews = reviews.length;
+            p.image = p.images[0];
+            delete p.images;
+            return p;
+          })
+          .catch(err => res.send(err));
+      });
+      Promise.all(places)
+        .then(data => res.send(data))
+        .catch(err => res.send(err));
+    })
     .catch(err => res.send(err));
 };
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const database = require("./database.js");
-
+const cors = require("cors");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,10 +11,9 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Welcome to Airbnb API");
 });
-
-app.get("/places", require("./controllers/getPlace"));
+app.use(cors({ credentials: true }));
+app.get("/places", require("./controllers/getPlaces"));
 app.post("/places", require("./controllers/postPlace"));
-app.get("/places/:id", require("./controllers/getPlace"));
 app.patch("/places/:id", require("./controllers/patchPlace"));
 app.delete("/places/:id", require("./controllers/deletePlace"));
 app.post("/type", require("./controllers/postType"));
@@ -24,7 +23,7 @@ app.post("/amenities", require("./controllers/postAmenities"));
 app.get("/amenities", require("./controllers/getAmenities"));
 
 app.post("/reviews", require("./controllers/postReviews"));
-app.get("/reviews/:id", require("./controllers/getReviews"));
+app.get("/reviews", require("./controllers/getReviews"));
 
 app.listen(4000, () => {
   console.log("Ready on port 4000");
