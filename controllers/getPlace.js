3d3735f7f0e98ae439576822ce68c4635342116b
ruthw <rwustenberg@gmail.com>
2019-09-16 00:58:47 -0500

    130102

--- controllers/getPlace.js
+++ controllers/getPlace.js
@@ -1,23 +1,38 @@
 const Place = require("../models/place");
+const Review = require("../models/review");
+const Type = require("../models/type");
 
 module.exports = (req, res) => {
   Place.findOne({ _id: req.params.id })
     .populate("type")
-    .populate({ path: "user", select: "name avatar" })
-    .then(data => {
-      res.send(data);
+    .populate({ path: "host", select: "name avatar" })
+    .lean()
+    .then(place => {
+      Review.find({ place: place._id })
+        .populate("author")
+        .lean()
+        .then(reviews => {
+          place.reviews = reviews;
+          place.image = place.images[0];
+          res.send(place);
+        });
     })
     .catch(err => {
       res.send(err);
     });
 };
-// module.exports = (req, res) => {
-//   console.log(req.query);
-//   Place.find(req.query)
-//     .then(data => {
-//       res.send(data);
-//     })
-//     .catch(err => {
-//       res.send(err);
-//     });
-// };
+// .then(data => {
+// 	let places = data.map(p => {
+// 		return Review.find({ place: p._id })
+// 			.then(reviews => {
+// 				p.reviews = reviews.length;
+// 				p.image = p.images[0];
+// 				delete p.images;
+// 				return p;
+// 			})
+// 			.catch(err => res.send(err));
+// 	});
+// 	Promise.all(places)
+// 		.then(data => res.send(data))
+// 		.catch(err => res.send(err));
+// })
---
+++ controllers/postSignup.js
@@ -0,0 +1,14 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+module.exports = (req, res) => {
+  req.body.password = bcrypt.hashSync(req.body.password, 10);
+  console.log("encrypted", encrypted);
+  User.create(req.body)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+};
