e318180f0b2893006973708810865005fb6f5644
ruthw <rwustenberg@gmail.com>
2019-09-16 03:39:49 -0500

    120105

--- controllers/postSignup.js
+++ controllers/postSignup.js
@@ -1,14 +1,19 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res) => {
   req.body.password = bcrypt.hashSync(req.body.password, 10);
   console.log("encrypted", encrypted);
   User.create(req.body)
-    .then(data => {
-      res.send(data);
+    .then(user => {
+      let obj = user.toObject();
+      let token = jwt.sign(obj, "secretCode");
+      console.log("token", token);
+      res.send(token);
     })
     .catch(err => {
+      console.log(err);
       res.send(err);
     });
 };
