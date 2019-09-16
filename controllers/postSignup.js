70765f953d5b75ac590860e98b18452e5b539ddf
ruthw <rwustenberg@gmail.com>
2019-09-16 05:27:36 -0500

    120108

--- controllers/postSignup.js
+++ controllers/postSignup.js
@@ -5,6 +5,19 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res) => {
   req.body.password = bcrypt.hashSync(req.body.password, 10);
   console.log("encrypted", encrypted);
+  User.findOne({ email: req.body.email }).lean()
+	.then(user => {
+		let match = bcrypt.compareSync(req.body.password, user.password)
+			if(match){
+				delete user.password
+				res.send(user)} else {res.send("wrong!!")}
+			}).catch(err => {
+				console.log(err)
+				res.send(err)
+			})
+		}
+
+
   User.create(req.body)
     .then(user => {
       let obj = user.toObject();
